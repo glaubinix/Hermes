59186a85f29571d8404699980ce553db1ba0b58c
Stephan Vock <stephan.vock@innogames.de>
2013-09-25 21:42:38 +0200

    replaced fetching of repositories with guzzle client script does not return the repository anymore but redirects

--- index.php
+++ index.php
@@ -1,19 +1,24 @@
 <?php
 
+use Guzzle\Http\Client;
+use Guzzle\Http\EntityBody;
+
+include 'vendor/autoload.php';
+
 if (!is_dir('dumps/')) {
 	mkdir('dumps/');
 }
 
-$file = base64_decode($_GET['file']);
+$file = urldecode($_GET['file']);
 
 $file_identifier = sha1($file);
-
-echo 'loading: ' . $file . "\n";
-if (!file_exists('dumps/' . $file_identifier)) {
-	echo 'storing: ' . $file . "\n";
-	$content = file_get_contents($file);
-	file_put_contents('dumps/' . $file_identifier , $content);
+$to = 'dumps/' . $file_identifier;
+if (!file_exists($to)) {
+	$client = new Client($file);
+	$request = $client->get();
+	$response_body = EntityBody::factory(fopen($to, 'w+'));
+	$request->setResponseBody($response_body);
+	$request->send();
 }
 
-echo file_get_contents('dumps/' . $file_identifier);
-
+header(sprintf("location: %s", $to));
